30324e41e11b19ce490c5e2bac75999fc667e601
Patroklos <bc791925@ens.uqam.ca>
2023-08-03 23:53:27 -0400

    minor refactor

--- Ville_Montreal_Frontend/src/app/app.module.ts
+++ Ville_Montreal_Frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { SearchByComponent } from './search-by/search-by.component';
 import { QuickSearchComponent } from './quick-search/quick-search.component';
 import { InspectionComponent } from './inspection/inspection.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
     SearchByComponent,
     QuickSearchComponent,
-    InspectionComponent
+    InspectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
